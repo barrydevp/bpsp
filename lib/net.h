b373b36aaf78e15463e868e2970dd00d0ea88a97
barrydevp <barrydevp@gmail.com>
2021-11-13 23:15:39 +0700

    feat(lib): WIP net

---
+++ lib/net.h
@@ -0,0 +1,45 @@
+#ifndef _NET_H_
+#define _NET_H_
+
+#include <arpa/inet.h>
+
+#include "datatype.h"
+#include "status.h"
+
+#define BPSP_NET_BACKLOG 20
+
+typedef enum { NET_T_NOOP = -1, NET_T_PASSIVE = 0, NET_T_ACTIVE = 1 } net__type;
+
+typedef enum {
+    /** both **/
+    NET_S_INIT = -1,
+    NET_S_DISCONNECTED = 0,  ///< The connection has been disconnected
+    NET_S_CLOSED,            ///< The connection is closed
+
+    /** active connection  **/
+    NET_S_CONNECTING,    ///< The connection is in the process or connecting
+    NET_S_CONNECTED,     ///< The connection is connected
+    NET_S_RECONNECTING,  ///< The connection is in the process or reconnecting
+
+    /**  passive connection **/
+    NET_S_LISTEN,
+
+} net__state;
+
+typedef struct {
+    int sockfd;
+    struct sockaddr_in* addr;
+
+    net__type type;
+
+    net__state state;
+} bpsp__connection;
+
+bpsp__connection* net__connect(const char* host, uint16_t port);
+bpsp__connection* net__listen(const char* host, uint16_t port);
+bpsp__connection* net__accept(bpsp__connection* listener);
+status__err net__close(bpsp__connection* conn);
+status__err net__read(bpsp__connection* conn, void* buf, size_t count, size_t *n_read, uint8_t block);
+status__err net__write(bpsp__connection* conn, void* buf, size_t count, size_t *n_write, uint8_t block);
+
+#endif  // _NET_H_
