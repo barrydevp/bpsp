b11435032d0f497ce0a30bf2bfcdddbc16daef85
barrydevp <barrydevp@gmail.com>
2021-11-19 17:17:15 +0700

    feat(lib): adding util (rand_str)

---
+++ lib/util.c
@@ -0,0 +1,13 @@
+#include "util.h"
+
+void rand_str(char* str, size_t size) {
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    if (str && size) {
+        --size;
+        for (size_t n = 0; n < size; n++) {
+            int key = rand() % (int)(sizeof charset - 1);
+            str[n] = charset[key];
+        }
+        str[size] = '\0';
+    }
+}
---
+++ lib/util.h
@@ -0,0 +1,9 @@
+#ifndef _UTIL_H_
+#define _UTIL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void rand_str(char* str, size_t size);
+
+#endif  // _UTIL_H_
