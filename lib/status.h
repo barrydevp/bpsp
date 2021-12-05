1d76b44f52e14932d51ad5dec3893ee58e91b3cb
barrydevp <barrydevp@gmail.com>
2021-12-05 22:43:31 +0700

    feat(lib): update status__error

--- lib/status.h
+++ lib/status.h
@@ -32,8 +32,7 @@ typedef enum {
     BPSP_PROTOCOL_ERROR,   ///< Error when parsing a protocol message,
                            ///  or not getting the expected message.
     BPSP_IO_ERROR,         ///< IO Error (network communication).
-    BPSP_MAX_VAR_HEADERS,  ///< The protocol message read from the socket
-                           ///  does not fit in the read buffer.
+    BPSP_MAX_VAR_HEADERS,  ///< Maximum variable heaaders
 
     BPSP_CONNECTION_CLOSED,           ///< Operation on this connection failed because
                                       ///  the connection is closed.
@@ -65,27 +64,22 @@ typedef enum {
     BPSP_TREE_DUP_SUBSCRIBER,  ///< Found duplicate subscriber in node_tree
     BPSP_INVALID_VAR_HEADERS,  ///< Invalid variable headers.
 
-    BPSP_ILLEGAL_STATE,  ///< An unexpected state, for instance calling
-                         ///  #natsSubscription_NextMsg() on an asynchronous
-                         ///  subscriber.
+    BPSP_ILLEGAL_STATE,  ///< An unexpected state
 
-    BPSP_SLOW_CONSUMER,  ///< The maximum number of messages waiting to be
-                         ///  delivered has been reached. Messages are dropped.
+    BPSP_SLOW_SUBSCRIBER,  ///< The maximum number of messages waiting to be
+                           ///  delivered has been reached. Messages are dropped.
 
     BPSP_MAX_PAYLOAD,         ///< Attempt to send a payload larger than the maximum
-                              ///  allowed by the NATS Server.
-    BPSP_MAX_DELIVERED_MSGS,  ///< Attempt to receive more messages than allowed, for
-                              ///  instance because of #natsSubscription_AutoUnsubscribe().
+                              ///  allowed by the BPSP Server.
+    BPSP_MAX_DELIVERED_MSGS,  ///< Attempt to receive more messages than allowed
 
     BPSP_INSUFFICIENT_BUFFER,  ///< A buffer is not large enough to accommodate the data.
 
-    BPSP_NO_MEMORY,  ///< An operation could not complete because of insufficient
-                     ///  memory.
+    BPSP_NO_MEMORY,  ///< An operation could not complete because of insufficient memory
 
     BPSP_SYS_ERROR,  ///< Some system function returned an error.
 
-    BPSP_TIMEOUT,  ///< An operation timed-out. For instance
-                   ///  #natsSubscription_NextMsg().
+    BPSP_TIMEOUT,  ///< An operation timed-out.
 
     BPSP_FAILED_TO_INITIALIZE,  ///< The library failed to initialize.
     BPSP_NOT_INITIALIZED,       ///< The library is not yet initialized.
@@ -95,23 +89,17 @@ typedef enum {
 
     BPSP_NO_SERVER_SUPPORT,  ///< The server does not support this action.
 
-    BPSP_NOT_YET_CONNECTED,  ///< A connection could not be immediately established and
-                             ///  #natsOptions_SetRetryOnFailedConnect() specified
-                             ///  a connected callback. The connect is retried asynchronously.
+    BPSP_NOT_YET_CONNECTED,  ///< A connection wasn't established
+    BPSP_DRAINING,           ///< A connection and/or subscription entered the draining mode.
+                             ///  Some operations will fail when in that mode.
 
-    BPSP_DRAINING,  ///< A connection and/or subscription entered the draining mode.
-                    ///  Some operations will fail when in that mode.
-
-    BPSP_INVALID_QUEUE_NAME,  ///< An invalid queue name was passed when creating a queue
-                              ///< subscription.
+    BPSP_INVALID_GROUP_NAME,  ///< An invalid group name was passed when creating a group subscription
 
     BPSP_NO_RESPONDERS,  ///< No responders were running when the server received the request.
 
-    BPSP_MISMATCH,  ///< For JetStream subscriptions, it means that a consumer sequence mismatch was
-                    ///< discovered.
+    BPSP_MISMATCH,  ///< Request mismatch
 
-    BPSP_MISSED_HEARTBEAT,  ///< For JetStream subscriptions, it means that the library detected that
-                            ///< server heartbeats have been missed.
+    BPSP_MISSED_HEARTBEAT,  ///< For Heartbeat missed
 
 } status__err;
 
